5c3223e69f384a2076441d1c481dcddbae1df6fe
zawacka-agnieszka <44145312+zawacka-agnieszka@users.noreply.github.com>
2018-10-14 22:56:09 +0200

    add my code

---
+++ 8.2_storefunctions.js
@@ -0,0 +1,13 @@
+//program wyświetlający za pomocą typowej funkcji
+//informacje o sklepie U Zosi
+	
+	function storeInfo() //store info = zapisz informacje
+	{
+		document.write("Witamy U Zosi<br>");
+		document.write("ul. Szczytnicka 26<br>");
+		document.write(" 50-382 Wrocław<br>");
+		document.write("Czynne codziennie od 7.00 do 22.00<br>");
+		document.write("Telefon 11-234-567-889<br>");
+		document.write("Odwiedź naszą witrynę: www.uzosi.pl<br>");
+	}
+	// <script src='8.2_funkcje_sklepUzosi__#2.js'></script>
